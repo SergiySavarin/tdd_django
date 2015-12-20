f05dff1a392c885959268936a2a9649fdbdd0189
SergiySavarin <vir.host@gmail.com>
2015-12-20 21:25:01 +0200

    initial

---
+++ fts/tests.py
@@ -0,0 +1,17 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+
+class PollsTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_create_new_poll_via_admin_site(self):
+        self.browser.get(self.live_server_url + '/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+        self.fail('finish this test')
