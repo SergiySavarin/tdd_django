44bc5a3e94d41d94bfd6d0e8355d4ba746caf63e
SergiySavarin <vir.host@gmail.com>
2015-12-22 23:27:16 +0200

    del not needed comments

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -46,17 +46,7 @@ class NewVisitorTest(LiveServerTestCase, TestCase):
         self.assertRegexpMatches(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # import time
-        # time.sleep(10)
-
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
-        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-
         # There is still a text box inviting her to add another item.
-        # She enters "Use peacock feathers to make a fly" (Edith is 
-        # very methodical)
-        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
@@ -97,8 +87,5 @@ class NewVisitorTest(LiveServerTestCase, TestCase):
         # Edith wonders wether the site will remember her list. Then she
         # sees that site has generated a unique URL for her -- there is 
         # some explanatory text to that effect.
-        # self.fail('Finish the test!')
 
-        # She visits this URL - her to-do list is still there.
-
-        # Satisfied, she goes to sleep
+        # Satisfied, they both go back to sleep
