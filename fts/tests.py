561201b5ee6150038180d0c4b90e1b79c660e124
SergiySavarin <vir.host@gmail.com>
2015-12-22 17:05:20 +0200

    Model for fts items and associated migration

--- fts/tests.py
+++ fts/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from fts.models import Item
 from fts.views import home_page
 from helper import ignore_token
 
@@ -33,3 +34,22 @@ class HomePageTest(TestCase):
         )
         content = ignore_token(response.content.decode())
         self.assertEqual(content, expected_html)
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retriving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
