5b103addac06380578eea01e2c60259919604e3d
SergiySavarin <vir.host@gmail.com>
2015-12-23 11:55:54 +0200

    new url, view for adding to exist list. func test passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,8 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from unittest import TestCase
 
-class NewVisitorTest(LiveServerTestCase, TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -12,10 +11,12 @@ class NewVisitorTest(LiveServerTestCase, TestCase):
     def tearDown(self):
         self.browser.quit()
 
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
